2eea2b9d358993482ab4f2096ce1242b4b514b56
remslamjel <remk948@gmail.com>
2023-03-13 17:18:04 +0530

    dot env api secured

--- src/requests.js
+++ src/requests.js
@@ -1,14 +1,12 @@
-const APIKEY = "";
-
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en=US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
-  fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en=US`,
-  fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${APIKEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${APIKEY}&with_genres=10749`,
-  fetchDocumentaries: `/discover/movie?api_key=${APIKEY}&with_genres=99`,
+  fetchTrending: `/trending/all/week?api_key=${process.env.REACT_APP_APIKEY}&language=en=US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${process.env.REACT_APP_APIKEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${process.env.REACT_APP_APIKEY}&language=en=US`,
+  fetchActionMovies: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=99`,
 };
 
 export default requests;
